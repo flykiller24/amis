32c459ddb472b039dd4106e4e1f5d8b1907ccb8d
flykiller24 <43314274+flykiller24@users.noreply.github.com>
2018-12-26 12:53:56 +0200

    Add files via upload

--- km-82/Starov_Denys/workshop1/homework/task1.py
+++ km-82/Starov_Denys/workshop1/homework/task1.py
@@ -1,7 +1,37 @@
-"""
-Тут написати умову до завдання
-"""
+import re
 
+def num_val_int():
 
+    user_input = input("Enter int number: ")
 
-#TODO
+    if (re.match(r"^\d+$", user_input)):
+        return user_input
+    else:
+        return False
+
+def num_val_float():
+
+    user_input = input("Enter float number: ")
+
+    if (re.match(r"^\d+\.\d+$", user_input)):
+        return user_input
+    else:
+        return False
+
+def inputInt():
+    intNum = num_val_int()
+    while not intNum:
+        print("Error")
+        intNum = num_val_int()
+    return intNum
+
+def inputFloat():
+    floatNum = num_val_float()
+    while not floatNum:
+        print("Error")
+        floatNum = num_val_float()
+    return floatNum
+
+if  __name__ == "__main__":
+    inputInt()
+    inputFloat()
